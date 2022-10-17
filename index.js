c07df51d36d91b3f3485f2e6c2edc490dd8a87cf
Navin Patil <navinpatilwork@gmail.com>
2022-10-17 16:51:05 +0530

    Auth and user model

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose
-.connect(process.env.MONGO_URL)
-.then(() => console.log("DB Connection Successfull!"))
-.catch((err) => {
-  console.log(err);
-});
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
 //app.use("/api/auth", authRoute);
-  app.listen(process.env.PORT || 5000, () => {
-    console.log("Backend server is running!");
-  });
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running! " + process.env.PORT);
+});
---
+++ routes/auth.js
@@ -0,0 +1,68 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  console.log("registration");
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+    // return;
+  } catch (err) {
+    res.status(500).json(err);
+    // return;
+  }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  // console.log("login");
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json("Wrong credentials!");
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    if(OriginalPassword !== req.body.password){
+      return res.status(401).json("Wrong Password!");
+    }
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
+    console.log("login");
+    // return;
+  } catch (err) {
+    res.status(500).json(err);
+    // return;
+  }
+});
+
+module.exports = router;

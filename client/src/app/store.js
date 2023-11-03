8dc3ae33e4843e6c4b7139a7e3b5030801a623e1
Navin Patil <navinpatilwork@gmail.com>
2023-11-03 16:12:19 +0530

    working StockSlice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,3 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { stockSlice } from "../features/stockSlice";
 
-export const store = configureStore({});
+// const configureStore = require("@reduxjs/toolkit").configureStore;
+// const stockReducer = require("../features/stockSlice");
+
+const store = configureStore({
+  reducer: {
+    stock: stockSlice.reducer,
+  },
+});
+
+// module.exports = store;
+export default store;

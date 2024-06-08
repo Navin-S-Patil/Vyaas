2cfddbbb29b762617532411545a6f6fe94f48d3f
Navin Patil <navinpatilwork@gmail.com>
2024-06-08 22:29:57 +0530

    config

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // The URL of your frontend
+    origin: "https://vyaas-client.onrender.com", // The URL of your frontend
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
 );

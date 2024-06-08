a0d0ec673f8b64ab02332d189fa2962e367bfe36
Navin Patil <navinpatilwork@gmail.com>
2024-06-08 22:04:51 +0530

    small changes

--- routes/addCredits.js
+++ routes/addCredits.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const protect = require("../middleware/protect");
+const protect = require("../middleware/authMiddleware");
 
 router.put("/addcredits", protect, (req, res) => {
   // Add credits to user account
